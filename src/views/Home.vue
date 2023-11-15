b94a155e892c372cd94ff5c2b9824ec92ce7c56d
RainBowT <rainbowt0506@gmail.com>
2023-11-15 22:01:37 +0800

    [feat] Implement alphabetical navigation links on the home page using router-links.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link to="/" v-for="letter of letters">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,4 +19,5 @@ import { computed } from "vue";
 import store from "../store";
 
 const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>
