103f2a25533c97fba5d431dab6760aac25343eb3
RainBowT <rainbowt0506@gmail.com>
2023-11-18 16:00:01 +0800

    [feat] Implement MealsByIngredient & Ingredients

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from "../views/Home.vue";
 import MealList from "../views/MealList.vue";
 import MealsByName from "../views/MealsByName.vue";
 import MealsByLetter from "../views/MealsByLetter.vue";
+import Ingredients from "../views/Ingredients.vue";
 import MealsByIngredient from "../views/MealsByIngredient.vue";
 import MealDetails from "../views/MealDetails.vue";
 
@@ -27,6 +28,11 @@ const routes = [
         path: "/by-letter/:letter?",
         name: "byLetter",
         component: MealsByLetter,
+      },
+      {
+        path: "/ingredients",
+        name: "ingredients",
+        component: Ingredients,
       },
       {
         path: "/by-ingredient/:ingredient?",
